9c8ddccc7861fcd2b5f6f20fcb9b431b60721580
Faustino Zanetto <faustizanetto@gmail.com>
2022-07-15 12:11:19 -0300

    Modified hero section, now using a video as background and it spans the whole screen.

--- src/modules/views/home/hero/home-hero.tsx
+++ src/modules/views/home/hero/home-hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Section from '@modules/section/section';
-import { Text, Box, VStack, HStack, Button, Image, Grid } from '@chakra-ui/react';
+import { Text, Box, VStack, HStack, Button, Image, Grid, Container, Heading } from '@chakra-ui/react';
 
 interface IHomeHeroProps {}
 
@@ -8,36 +8,71 @@ const HomeHero: React.FC<IHomeHeroProps> = (props) => {
   const {} = props;
 
   return (
-    <Section backgroundColor="black">
-      <Grid gap={6} gridTemplateColumns="1fr 1fr">
-        {/* Left */}
-        <VStack height="full" spacing={4} alignItems="flex-start">
-          {/* Texts */}
-          <Text as="h1" fontSize="4.5em" fontWeight={700} color="white" lineHeight={1}>
-            Experience the true taste of Coffee.
-          </Text>
-          <Text as="h2" fontSize="2em" fontWeight={400} color="white">
-            Our homebrew coffee is made with the best ingredients and the best brewing process in the city.
-          </Text>
+    <Box display="flex" alignItems="center" justifyContent="center" height="100vh" width="100%" position="relative">
+      {/* Background Video */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          height: '100vh',
+          objectFit: 'cover',
+        }}
+      >
+        <source src="/assets/videos/hero-video.mp4" type="video/mp4" />
+      </video>
 
-          {/* Buttons */}
-          <HStack spacing={6} paddingTop={8}>
-            <Button size="lg">Reserve now</Button>
-            <Button variant="ghost" size="lg">
-              See our menu
-            </Button>
-          </HStack>
-        </VStack>
+      {/* Content */}
+      <Box zIndex={1}>
+        <Box paddingX={{ base: 4, md: 8 }} paddingY="8rem">
+          <Container maxWidth="8xl">
+            <Grid gap={6} gridTemplateColumns="1fr 1fr">
+              {/* Left */}
+              <VStack height="full" spacing={4} alignItems="flex-start">
+                {/* Texts */}
+                <Heading fontWeight={800} color="white" fontSize="7xl">
+                  Experience the
+                  <br />
+                  <Text
+                    as="span"
+                    position="relative"
+                    _after={{
+                      content: "''",
+                      width: 'full',
+                      height: '30%',
+                      position: 'absolute',
+                      bottom: 1,
+                      left: 0,
+                      bg: 'orange.400',
+                      zIndex: -1,
+                    }}
+                  >
+                    True Taste
+                  </Text>
+                  <br />
+                  <Text as="span">Of Coffee</Text>
+                </Heading>
+                <Text as="h2" fontSize="2em" fontWeight={400} color="white">
+                  Our homebrew coffee is made with the best ingredients and the best brewing process in the city.
+                </Text>
 
-        {/* Right */}
-        <Box height="full">
-          <Image
-            src="https://www.losdoscristianos.com/uploads/5/7/5/8/57580401/los-dos-website-vertical-coffee-cup1_orig.jpg"
-            alt="Hero Image"
-          />
+                {/* Buttons */}
+                <HStack spacing={6} paddingTop={8}>
+                  <Button size="lg">Reserve now</Button>
+                  <Button variant="ghost" size="lg">
+                    See our menu
+                  </Button>
+                </HStack>
+              </VStack>
+            </Grid>
+          </Container>
         </Box>
-      </Grid>
-    </Section>
+      </Box>
+    </Box>
   );
 };
 
